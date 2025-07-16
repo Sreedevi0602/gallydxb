b5806b74eb9213b3b3671df106e807190a652cfd
Sreedevi0602 <sreedevivs1002@gmail.com>
2025-07-17 01:14:50 +0530

    mobile menu dropdown fixed. Favicon added. Unnecessary files deleted.

--- gallydxb/static/js/header_scroll.js
+++ gallydxb/static/js/header_scroll.js
@@ -8,21 +8,4 @@ window.addEventListener("scroll", function () {
     });
 
 
-  window.addEventListener("scroll", function () {
-    if (window.scrollY > 100) {
-      header.classList.add("header-scrolled");
-    } else {
-      header.classList.remove("header-scrolled");
-    }
-  });
-
-  burger.addEventListener('click', () => {
-    if (header.classList.contains('header-scrolled')) {
-      menu.classList.add('menu-scrolled');
-    } else {
-      menu.classList.remove('menu-scrolled');
-    }
-
-    menu.classList.add('active');
-    document.body.classList.add('menu-open');
-  });
+  
